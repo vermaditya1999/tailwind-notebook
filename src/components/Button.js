428d8c542102383de64b6d7e0abbe25c2b69fb58
Aditya Verma <aditya17009@iiitd.ac.in>
2022-10-18 22:19:27 +0530

    basic structure

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+export default function Button() {
+	return (
+		<div>
+			<button className='px-2 py-1 border shadow'>Click Me</button>
+		</div>
+	);
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+export default function Home() {
+	return (
+		<div className='p-4'>
+			<div>
+				<div>
+					<h1 className='text-2xl font-bold'>Tailwind notebook</h1>
+					<p className='text-gray-600'>
+						My personal tailwind playground. Because designing with tailwind is{' '}
+						<span className='text-red-600'>&hearts;</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
